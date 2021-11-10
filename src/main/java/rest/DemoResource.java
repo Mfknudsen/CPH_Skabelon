ea5a7e167a3aeab94f108fcc9747b8e6e6ae7e0c
Mads Knudsen <35879356+Mfknudsen@users.noreply.github.com>
2021-11-10 13:41:03 +0100

    Added utils.HttpUtils

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -1,12 +1,9 @@
 package rest;
 
 import com.google.gson.Gson;
-import entities.User;
-import java.util.List;
+
 import javax.annotation.security.RolesAllowed;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -14,14 +11,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
+
 import utils.EMF_Creator;
+import utils.HttpUtils;
 
 /**
  * @author lam@cphbusiness.dk
  */
 @Path("info")
 public class DemoResource {
-    
+
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
     @Context
     private UriInfo context;
@@ -30,26 +29,20 @@ public class DemoResource {
     SecurityContext securityContext;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     public String getInfoForAll() {
-        return "{\"msg\":\"Hello anonymous\"}";
-    }
-
-    //Just to verify if the database is setup
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("all")
-    public String allUsers() {
-
-        EntityManager em = EMF.createEntityManager();
         try {
-            TypedQuery<User> query = em.createQuery ("select u from User u",entities.User.class);
-            List<User> users = query.getResultList();
-            return "[" + users.size() + "]";
-        } finally {
-            em.close();
+            String result = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
+            result += "\n";
+            result += HttpUtils.fetchData("https://icanhazdadjoke.com");
+            result += "\n";
+
+            return result;
+        } catch (Exception e) {
+            return "Failed";
         }
     }
+
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
